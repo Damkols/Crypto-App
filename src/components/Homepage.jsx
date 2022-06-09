7852ae8d955cffbf5b35affde054eae6e28ac8bc
damkols <damolausmankolapo@gmail.com>
2022-06-09 11:47:14 -0800

    porject structure

---
+++ src/components/Homepage.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import milify from "millify";
+import { Typography, Row, Col, Statistic } from "antd";
+import { Link } from "react-router-dom";
+
+const { Title } = Typography;
+
+const Homepage = () => {
+  return (
+    <div>
+      <Title level={2}>Homepage</Title>
+    </div>
+  );
+};
+
+export default Homepage;
